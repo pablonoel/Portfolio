bf118654e641a58503cdef5b0e83b4d57c30c960
Pablo Noel <pablo@next-solutions.ca>
2026-05-05 01:36:24 -0400

    improvements, text and  bug fixings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ import sitemap from '@astrojs/sitemap'
 export default defineConfig({
   site: 'https://www.pablonoel.com',
   integrations: [react(), sitemap()],
+  compressHTML: true,
+  vite: {
+    build: {
+      minify: 'esbuild',
+      cssMinify: 'esbuild',
+    },
+  },
 })
